aaa5940e245f5a92f449258d664b87da9835d6ac
Simon Raschke <simon.raschke@outlook.de>
2018-02-05 17:28:10 +0100

    ADDED HighFive HDF5 High Level library and minimum example

---
+++ HighFive/include/highfive/bits/H5Utils.hpp
@@ -0,0 +1,161 @@
+/*
+ *  Copyright (c), 2017, Adrien Devresse <adrien.devresse@epfl.ch>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
+#ifndef H5UTILS_HPP
+#define H5UTILS_HPP
+
+// internal utilities functions
+#include <cstddef> // __GLIBCXX__
+#include <exception>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+#ifdef H5_USE_BOOST
+#include <boost/multi_array.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#endif
+
+#ifndef H5_USE_CXX11
+#if ___cplusplus >= 201103L
+#define H5_USE_CXX11 1
+#else
+#define H5_USE_CXX11 0
+#endif
+#endif
+
+namespace HighFive {
+
+namespace details {
+
+// determine at compile time number of dimensions of in memory datasets
+template <typename T>
+struct array_dims {
+    static const size_t value = 0;
+};
+
+template <typename T>
+struct array_dims<std::vector<T> > {
+    static const size_t value = 1 + array_dims<T>::value;
+};
+
+template <typename T>
+struct array_dims<T*> {
+    static const size_t value = 1 + array_dims<T>::value;
+};
+
+template <typename T, std::size_t N>
+struct array_dims<T[N]> {
+    static const size_t value = 1 + array_dims<T>::value;
+};
+
+#ifdef H5_USE_BOOST
+template <typename T, std::size_t Dims>
+struct array_dims<boost::multi_array<T, Dims> > {
+    static const size_t value = Dims;
+};
+
+template <typename T>
+struct array_dims<boost::numeric::ublas::matrix<T> > {
+    static const size_t value = 2;
+};
+#endif
+
+// determine recursively the size of each dimension of a N dimension vector
+template <typename T>
+void get_dim_vector_rec(const T& vec, std::vector<size_t>& dims) {
+    (void)dims;
+    (void)vec;
+}
+
+template <typename T>
+void get_dim_vector_rec(const std::vector<T>& vec, std::vector<size_t>& dims) {
+    dims.push_back(vec.size());
+    get_dim_vector_rec(vec[0], dims);
+}
+
+template <typename T>
+std::vector<size_t> get_dim_vector(const std::vector<T>& vec) {
+    std::vector<size_t> dims;
+    get_dim_vector_rec(vec, dims);
+    return dims;
+}
+
+// determine at compile time recursively the basic type of the data
+template <typename T>
+struct type_of_array {
+    typedef T type;
+};
+
+template <typename T>
+struct type_of_array<std::vector<T> > {
+    typedef typename type_of_array<T>::type type;
+};
+
+#ifdef H5_USE_BOOST
+template <typename T, std::size_t Dims>
+struct type_of_array<boost::multi_array<T, Dims> > {
+    typedef typename type_of_array<T>::type type;
+};
+
+template <typename T>
+struct type_of_array<boost::numeric::ublas::matrix<T> > {
+    typedef typename type_of_array<T>::type type;
+};
+#endif
+
+template <typename T>
+struct type_of_array<T*> {
+    typedef typename type_of_array<T>::type type;
+};
+
+template <typename T, std::size_t N>
+struct type_of_array<T[N]> {
+    typedef typename type_of_array<T>::type type;
+};
+
+// check if the type is a container ( only vector supported for now )
+template <typename>
+struct is_container {
+    static const bool value = false;
+};
+
+template <typename T>
+struct is_container<std::vector<T> > {
+    static const bool value = true;
+};
+
+// check if the type is a basic C-Array
+// check if the type is a container ( only vector supported for now )
+template <typename>
+struct is_c_array {
+    static const bool value = false;
+};
+
+template <typename T>
+struct is_c_array<T*> {
+    static const bool value = true;
+};
+
+template <typename T, std::size_t N>
+struct is_c_array<T[N]> {
+    static const bool value = true;
+};
+
+
+// shared ptr portability
+// was used pre-C++11, kept for compatibility
+namespace Mem {
+    using namespace std;
+} // end Mem
+
+} // end details
+}
+
+#endif // H5UTILS_HPP
--- src/vesicle.cpp
+++ src/vesicle.cpp
@@ -22,15 +22,39 @@
 #include <tbb/task_scheduler_init.h>
 #include <tbb/task_arena.h>
 
-#include "vesicleIO/hdf5_handler.hpp"
+#undef H5_USE_BOOST
+#define H5_USE_BOOST
+#include <boost/multi_array.hpp>
+#include "H5File.hpp"
 
 
 int main(int argc, const char *argv[])
 {
-    HDF5Handler h;
-    h.setFileName("testfile.h5");
-    h.createDataset(H5::PredType::NATIVE_FLOAT,"testset",10000,9999);
-    h.writeToDataset("testset");
+    // we create a new hdf5 file
+    const unsigned long x = 100000000;
+    const unsigned long y = 2;
+
+    boost::multi_array<float, 2> my_array(boost::extents[x][y]);
+    for (size_t i = 0; i < x; ++i) {
+        for (size_t j = 0; j < y; ++j) {
+            my_array[i][j] = 1.337 + i + j;
+        }
+    }
+
+    HighFive::File file(boost::filesystem::system_complete("data.h5").string(), HighFive::File::ReadWrite | HighFive::File::Create | HighFive::File::Truncate);
+    HighFive::DataSet dataset = file.createDataSet<float>("/boost_multi_array", HighFive::DataSpace::From(my_array));
+    
+    // lets write our vector of int to the HDF5 dataset
+    dataset.write(my_array);
+
+    // read back
+    // boost::multi_array<int, 2> result;
+    // dataset.read(result);
+
+    // for(auto i : result)
+    // for(auto j : i)
+    // vesLOG(j)
+    
     return EXIT_SUCCESS;
 
     // register important signals in Controller bas class
