c9aeb29cd88233b4f6f02855e8e0e1e6f1d27473
Simon Raschke <simon.raschke@outlook.de>
2018-03-11 15:26:18 +0100

    ADDED: comments to plot scripts

--- plots/plot_helper_functions.py
+++ plots/plot_helper_functions.py
@@ -111,11 +111,12 @@ def getMatchedValues(filepath, param, constraints):
 # return all values for 'dir_path' 
 # in dicts that match all in 'constraints'
 # constraints mus be dict
-def getMatchedDirs(filepath, constraints):
+def getMatchedDirs(overviewfilepath, constraints):
     return getMatchedValues(filepath, "dir_path", constraints)
 
 
 
+# print an overview of a HDF5 file
 def fileOverviewHDF5(filepath):
     ph5 = pprint.PrettyPrinter(indent=4, compact=True, width=1)
     file = h5py.File(filepath, 'r')
@@ -128,20 +129,30 @@ def fileOverviewHDF5(filepath):
 
 
 
+# open a HDF5 dataset in @filepath
+# return np.array
+# if filepath doesnt exist return None
+# if Exception return empty list []
 def getHDF5Dataset(filepath, datasetname):
     if os.path.exists(filepath):
         print("HDF5 file exists:", filepath)
+
+        # try opening file
         try:
             FILE = h5py.File(filepath, 'r')
         except:
             print("cannot open file", filepath)
             return []
         # if datasetname in h5py.File(filepath, 'r'):
+
+        # try getting dataset
         try:
             dataset = FILE.get(datasetname).value.transpose()
         except:
             print("cannot open dataset", datasetname)
             return []
+
+        # check for data in dataset
         try:
             if dataset.ndim > 1:
                 return dataset
@@ -154,6 +165,8 @@ def getHDF5Dataset(filepath, datasetname):
 
 
 
+# open HDF5 dataset @datasetname at @filepath 
+# return attribute value for @attributename
 def getHDF5DatasetAttributes(filepath, datasetname, attributename):
     if os.path.exists(filepath):
         return h5py.File(filepath, 'r').get(datasetname).attrs[attributename]
@@ -162,39 +175,56 @@ def getHDF5DatasetAttributes(filepath, datasetname, attributename):
 
 
 
-def getAveragedData2D(filepath, datasetname, constraints):
+# open @overviewfilepath to look for dir_paths with given @constraints
+# open all @datasetname datasets (which must be 2D arrays) and average every field
+# return averaged [np.array]s
+def getAveragedData2D(overviewfilepath, datasetname, constraints):
     print(getAveragedData2D.__name__, "  get dataset", datasetname, "  with constraints", constraints)
-    dirs = getMatchedDirs(filepath,constraints)
+    # get dir_path's
+    dirs = getMatchedDirs(overviewfilepath,constraints)
+    # prepare datafilenames
     paths = [os.path.join(dir,"data.h5") for dir in dirs]
+    # load everythin into list of np.arrays
     all_data = [getHDF5Dataset(path,datasetname) for path in paths]
+    # put all columns of same number in buffers
     x_buffer = np.array([ data[0] for data in all_data ])
     y_buffer = np.array([ data[1] for data in all_data ])
     x, y = [], []
+    # iterate range from 0 to length of longest array in buffer
     for i in range(max([ len(array) for array in x_buffer ])):
         x_temp = []
+        # if possible add all elements of same number to temp
         for array in x_buffer:
             try:
                 x_temp.append(array[i])
             except IndexError:
                 pass
+        # and add the average to x
         x.append(np.average(x_temp))
+    # iterate range from 0 to length of longest array in buffer
     for i in range(max([ len(array) for array in y_buffer ])):
         y_temp = []
+        # if possible add all elements of same number to temp
         for array in y_buffer:
             try:
                 y_temp.append(array[i])
             except IndexError:
                 pass
+        # and add the average to y
         y.append(np.average(y_temp))
     return [x,y]
 
 
 
+# input 1D array and range of values
+# return all indexes to elements in value range
 def indexOfValueRange(column,value_range):
     return [ column.index(x) for x in column if x >= min(value_range) and x <= max(value_range) ]
 
 
 
+# input 2 1D arrays and get indexes to value range of x
+# return 2 arrays with the elements for given indexes
 def extractXValueRange(x,y,value_range):
     indexes = indexOfValueRange(x,value_range)
     new_x = [x[i] for i in indexes]
@@ -203,13 +233,10 @@ def extractXValueRange(x,y,value_range):
 
 
 
+# read HDF5 file @datafilepath
+# try and read system.box parameters from potential_energies dataset
+# if possible return volume else NaN
 def getVolume(datafilepath):
-    # print(getVolume.__name__)
-    # impossible this way, because of not reprinting config file
-    # box.[] will most likely be commented out and has value of 10
-    # x = getValues(overviewfilepath,"box.x")
-    # y = getValues(overviewfilepath,"box.y")
-    # z = getValues(overviewfilepath,"box.z")
     if not os.path.exists(datafilepath):
         raise AssertionError("{} does not exist".format(datafilepath))
     else:
@@ -223,6 +250,7 @@ def getVolume(datafilepath):
 
 
 
+# unused
 def getVolumes(filepath, constraints):
     print(getVolumes.__name__, "  with constraints", constraints)
     dirs = getMatchedDirs(filepath,constraints)
@@ -231,6 +259,8 @@ def getVolumes(filepath, constraints):
 
 
 
+# input hdf5 file object, datasetname and the searched size
+# return the number of occurencies of certain value (here: cluster size)
 def getClustersOfSize(datafile, datasetname, size):
     dataset = datafile.get(datasetname).value.transpose()
     unique, counts = np.unique(dataset[0], return_counts=True)
@@ -238,6 +268,10 @@ def getClustersOfSize(datafile, datasetname, size):
 
 
 
+# read hdf5 file @datafilepath
+# count occurencies of cluster @size
+# average for @time_range
+# return averaged value
 def getClustersOfSizeTimeAverage(datafilepath, size, time_range, clustergroup="/cluster_self_assembled"):
     # print(getClustersOfSizeTimeAverage.__name__, "in", datafilepath,"in time range", time_range)
     try:
@@ -245,33 +279,42 @@ def getClustersOfSizeTimeAverage(datafilepath, size, time_range, clustergroup="/
     except:
         return []
     values = []
+    # predict dataset names
+    # TODO: this is curcial for performance, time_range input is critical or no data will be found
     datasetnames = [clustergroup+"/time"+str(int(x)) for x in np.arange(int(min(time_range)),int(max(time_range))+1, 10000)]
     if int(min(time_range)) == int(max(time_range)):
         datasetnames = [clustergroup+"/time"+str(int(time_range[0]))]
     for datasetname in datasetnames:
         values.append(getClustersOfSize(file,datasetname,size)) 
-    return values
+    # return average if possible, else return zero
+    if len(values) > 0:
+        return np.average(values)
+    else:
+        return 0
 
 
-def getFreeParticleDensitySingleFile(datafilepath, time_range):
+
+# get the free particle density averaged over @time_range from @datafilepath
+def __detail_getFreeParticleDensity_single_simulation_datafile(datafilepath, time_range):
     # print(getFreeParticleDensitySingleFile.__name__, "in", datafilepath, "in time range", time_range)
     try:
         volume = getVolume(datafilepath)
     except AssertionError:
         return np.NaN
     data = getHDF5Dataset(datafilepath, "cluster_volumes")
-    if len(data) >= 1:
+    # if len(data) >= 1:
         # print("dataset has length", len(data))
-        inaccessible_volumes = extractXValueRange(list(data[0]),list(data[1]),time_range)[1]
-        inaccessible_volume = np.average(inaccessible_volumes)
-        free_particles = getClustersOfSizeTimeAverage(datafilepath, 1, time_range)
-        if len(free_particles) >= 1:
+    inaccessible_volumes = extractXValueRange(list(data[0]),list(data[1]),time_range)[1]
+    inaccessible_volume = np.average(inaccessible_volumes)
+    free_particles = getClustersOfSizeTimeAverage(datafilepath, 1, time_range)
+        # if len(free_particles) >= 1:
             # print("and has", free_particles, "free_particles")
-            free_particles = np.average(free_particles)
-            return float(free_particles)/(volume - inaccessible_volume)
+            # free_particles = np.average(free_particles)
+    return float(free_particles)/(volume - inaccessible_volume)
 
 
 
+# remove all None and !np.isfinite fields of array
 def removeBadEntries1D(x):
     if len(x) == 0:
         return []
@@ -284,6 +327,7 @@ def removeBadEntries1D(x):
 
 
 
+# remove all None and !np.isfinite fields of array
 def removeBadEntries2D(x,y):
     assert(len(x)==len(y))
     for i in reversed(range(len(x))):
@@ -298,6 +342,7 @@ def removeBadEntries2D(x,y):
 
 
 
+# get rho free from all files with given @constraints in @time_range
 def getFreeParticleDensity(overviewfilepath, constraints, time_range):
     print(getFreeParticleDensity.__name__, "  with constraints", constraints)
     dirs = getMatchedDirs(overviewfilepath,constraints)
@@ -305,7 +350,7 @@ def getFreeParticleDensity(overviewfilepath, constraints, time_range):
     # get theses values in parallel
     results = []
     for path in paths:
-        results.append(pool.apply_async(getFreeParticleDensitySingleFile,(path, time_range,)))
+        results.append(pool.apply_async(__detail_getFreeParticleDensity_single_simulation_datafile,(path, time_range,)))
     rho_free_values = removeBadEntries1D([r.get() for r in results if r.get() != None])
     # print("rho_free_values", rho_free_values)
     if len(rho_free_values) > 0:
