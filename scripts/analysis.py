fixed order calculation

scripts/analysis.py
@@ -34,6 +34,7 @@ parser.add_argument("--stop", type=float, default=10e20, help="starting time of 
 parser.add_argument("--forcenew", action='store_true', help="force new hdf5 file")
 parser.add_argument("--lowmem", action='store_true', help="dont save resname, saves memory BIG TIME")
 parser.add_argument("--timestats", action='store_true', help="show timer statistics")
+parser.add_argument("--reanalyze", action='store_true', help="reanalze from data.h5 file instead of trajectory")
 args = parser.parse_args()
 
 
@@ -43,7 +44,6 @@ np.set_printoptions(suppress=True)
 
 topology = args.top
 trajectory = args.traj
-
 
 
 # prepare input files
@@ -149,23 +149,26 @@ for snapshot in universe.trajectory:
     t_sub = time.perf_counter()
     # subcluster identification
     subcluster_labels = []
+    particledata["subcluster"] = -1
     for ID, group in particledata.groupby(["cluster"], as_index=False):
-        subcluster_labels.extend(helper.getSubclusterLabels(ID, group, args.clstr_eps))
+        subclusters = helper.getSubclusterLabels(ID, group, args.clstr_eps)
+        particledata.loc[group.index, "subcluster"] = subclusters
+        # subcluster_labels.extend(helper.getSubclusterLabels(ID, group, args.clstr_eps))
     # add the subcluster IDs
-    particledata["subcluster"] = subcluster_labels
+    # particledata["subcluster"] = subcluster_labels
     if args.timestats: print(f"subclstr took {time.perf_counter()-t_sub:.4f} seconds")
 
     t_shift = time.perf_counter()
     particledata["shiftx"] = particledata["x"]
     particledata["shifty"] = particledata["y"]
     particledata["shiftz"] = particledata["z"]
-    # shift subclusters towards largest subcluster
+    # shift subclusters towards largest subclusterr
     for ID, group in particledata.groupby("cluster"):
         newx, newy, newz = helper.getShiftedCoordinates(ID, group, args.clstr_eps, dimensions[:3])
         particledata.loc[newx.index, "shiftx"] = newx.values
         particledata.loc[newy.index, "shifty"] = newy.values
         particledata.loc[newz.index, "shiftz"] = newz.values
-    if args.timestats: print(f"shift took    {time.perf_counter()-t_shift:.4f} seconds")
+    if args.timestats: print(f"shift took    {time.perf_counte()-t_shift:.4f} seconds")
 
     t_order = time.perf_counter()
     # get the order of particle in cluster
@@ -201,5 +204,16 @@ for snapshot in universe.trajectory:
     t_end = time.perf_counter()
     print(f"time {snapshot.time} took {t_end-t_start:.4f} seconds")
     t_start = time.perf_counter()
+
+    # print(particledata)
+    print(particledata.groupby("clustersize")["order","epot"].mean())
+    # for size, group in particledata.groupby("clustersize"):
+    #     print(size, group["order"].mean())
+    #     if size > 30 and group["order"].mean() < 0 or size == 34:
+    #         print(group)
+    #         print(pd.concat([group['shiftx'], group['shifty'], group['shiftz']], axis=1).mean().values)
+    #         sys.exit()
+    # print(particledata[particledata["clustersize"] >= 30]["order"].mean())
+    # print(particledata[["clustersize","volume","epot","order"]].corr())
 
 datafile.close()

scripts/analysis_helper_functions.py
@@ -69,12 +69,12 @@ def getSubclusterLabels(ID, group, eps):
     #skip if noise
     if ID == -1:
         return np.zeros( len(group.index), dtype=int )
-    # arange a DBSCAN without PBC to get subclusters
-    coms_subcluster = pd.concat([group['x'], group['y'], group['z']], axis=1)
-    distances_array_subcluster = distance_array(coms_subcluster.values, coms_subcluster.values, box=None)
-    dbscan_subcluster = DBSCAN(min_samples=1, eps=eps, metric="precomputed", n_jobs=-1).fit(distances_array_subcluster)
-    # subcluster_labels.extend(dbscan_subcluster.labels_)
-    return dbscan_subcluster.labels_
+    else:
+        # arange a DBSCAN without PBC to get subclusters
+        coms_subcluster = pd.concat([group['x'], group['y'], group['z']], axis=1)
+        distances_array_subcluster = distance_array(coms_subcluster.values, coms_subcluster.values, box=None)
+        dbscan_subcluster = DBSCAN(min_samples=1, eps=eps, metric="precomputed", n_jobs=-1).fit(distances_array_subcluster)
+        return dbscan_subcluster.labels_
 
 
 
@@ -88,6 +88,11 @@ def getShiftedCoordinates(ID, group, eps, dimensions):
     centers = group.groupby("subcluster")['x','y','z'].mean()
     shifts = np.round(( -centers + centers.loc[max_subclusterID] )/dimensions[:3]).astype(int)
     shifts *= dimensions[:3]
+
+    coms_subcluster = pd.concat([group['x'], group['y'], group['z']], axis=1)
+    distances_array_subcluster = distance_array(coms_subcluster.values, coms_subcluster.values, box=None)
+    dbscan_subcluster = DBSCAN(min_samples=1, eps=eps, metric="precomputed", n_jobs=-1).fit(distances_array_subcluster)
+    np.set_printoptions(threshold=np.nan, linewidth=np.nan, precision=1)
     # calculate new coordinates based on shift
     newx = np.add(group["x"], shifts.loc[group["subcluster"]]["x"])
     newy = np.add(group["y"], shifts.loc[group["subcluster"]]["y"])